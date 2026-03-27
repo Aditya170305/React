8c6730db81554f5c74568da1c508a4bd582c4efe
Aditya170305 <adityagupta.bpl08@gmail.com>
2026-03-27 16:32:14 +0530

    Update

--- chatbot/src/App.jsx
+++ chatbot/src/App.jsx
@@ -1,11 +1,134 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+function ChatInput({ chatMessages , setChatMessages }){          //PascalCase
+
+        const [inputText , setInputText] = React.useState('');
+
+        function saveInputText(event){
+            setInputText(event.target.value);
+        }
+
+        function sendMessage(){
+
+            const newChatMessages = [
+                ...chatMessages,
+                {
+                    message : inputText,
+                    sender : "user",
+                    id : crypto.randomUUID()
+                }
+            ]
+
+            setChatMessages(newChatMessages);
+
+            const response = Chatbot.getResponse(inputText);
+            // console.log(response);
+            setChatMessages([
+                ...newChatMessages,
+                {
+                    message : response,
+                    sender : "robot",
+                    id : crypto.randomUUID()
+                }
+            ]);
+
+
+            setInputText('');
+        }
+
+            return (
+                // <div>
+                //     <input />
+                //     <button>Send</button>
+                // </div>
+                <div className = "chat-input-container">
+                    <input 
+                        placeholder = "Send a message to Chatbot" 
+                        size = "30"
+                        onChange = {saveInputText} 
+                        value = {inputText}
+                        className = "chat-input"
+                    />
+                    <button
+                        onClick = {sendMessage}
+                        className = "send-button" 
+                    >Send</button>
+                </div>
+            );
+        }
+
+        function ChatMessage({message , sender}){              //PascalCase
+            // console.log(props);
+            // const message = props.message;
+            // const sender  = props.sender;
+
+            // const {message , sender} = props;
+
+            // if(sender === "robot"){
+            //     return (
+            //         <div>
+            //             <img src = "robot.png" width = "80"/>
+            //             {message}
+            //         </div>
+            //     );
+            // }
+            return (    
+                <div className = {
+                    sender === "user" 
+                    ? "chat-message-user" 
+                    : "chat-message-robot"
+                }>
+                    {sender === "robot" && (
+                        <img src = "robot.png" 
+                        className = "chat-message-profile"
+                        />
+                    )}
+                    <div className = "chat-message-text">
+                        {message}
+                    </div>
+                    {sender === "user" && (
+                        <img src = "profile.png" 
+                        className = "chat-message-profile"
+                        />
+                    )}
+                </div>
+            );
+        }
+
+        function ChatMessages({ chatMessages }){
+
+            const chatMessagesRef = React.useRef(null);
+
+            React.useEffect(() => {
+                // console.log("updated");
+                const containerElem = chatMessagesRef.current;
+                if(containerElem){
+                    containerElem.scrollTop = containerElem.scrollHeight;
+                }
+            }, [chatMessages]);
+
+            return (
+                <div 
+                className = "chat-messages-container"
+                ref = {chatMessagesRef}
+                >
+                    {chatMessages.map((chatMessage) => {
+                        return (
+                            <ChatMessage 
+                            message = {chatMessage.message}
+                            sender = {chatMessage.sender}
+                            key = {chatMessage.id}
+                            />
+                            );
+                        })}
+                </div>
+                );
+            }
 
 function App() {
 
-            const [chatMessages , setChatMessages] = React.useState([{
+            const [chatMessages , setChatMessages] = useState([{
                 message : "hello chatbot",
                 sender : "user",
                 id : "id1"
